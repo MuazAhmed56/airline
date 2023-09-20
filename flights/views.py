0e4f37e8dd7ecb0d7248172a03f2c7adc8b3b14b
MuazAhmed56 <fauzanchaudhry92@gmail.com>
2023-09-21 04:20:32 +0500

    error with form

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import Flight, Passanger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -7,8 +9,17 @@ def index(request):
 # Flight class has a method objects which returns all the instances of the 
 # class Flight which happen to be all the flights.
 def flight(request,flight_id):
-    flights=Flight.objects.get(pk=flight_id)
+    flight=Flight.objects.get(pk=flight_id)
     #print(Flight.objects.get(pk=flight_id).origin) # for debugging
     #print(Flight.objects.get(pk=flight_id).destination) # for debugging
-    return render(request,"flights/flights.html", {"flights": flights.id,
-    "passangers":flights.passangers.all()})
+    return render(request,"flights/flights.html", {"flights": flight,
+    "passangers":flight.passangers.all(),
+    "non_passangers":Passanger.objects.exclude(flights=flight).all()})
+
+def book(request,flight_id):
+    if request.method=="POST":
+        flight=Flight.objects.get(pk=flight_id)
+        passanger=Passanger.objects.get(pk=int(request.POST["passengers"]))
+        
+        passanger.flights.add(flight)  #adding a new row in the table
+        return HttpResponseRedirect(reverse("flight",args=(flight.id,)))
